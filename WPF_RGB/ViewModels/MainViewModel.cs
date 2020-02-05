e2d2586f1dee6b6e2da77bd598c18f4edaf17e12
Michal Barth <u17.401@pslib.cloud>
2020-02-05 09:29:43 +0100

    Add project files.

---
+++ WPF_RGB/ViewModels/MainViewModel.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace WPF_RGB.ViewModels
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        private byte _red;
+        private byte _green;
+        private byte _blue;
+        private SolidColorBrush _color;
+
+        public MainViewModel()
+        {
+            Red = 255;
+            Green = 255;
+            Blue = 255;
+        }
+
+        public byte Red
+        {
+            get
+            {
+                return _red;
+            }
+            set
+            {
+                _red = value;
+                Colour = new SolidColorBrush(Color.FromArgb(255, Red, Green, Blue));
+                NotifyPropertyChanged();
+                NotifyPropertyChanged("MergedDecimal");
+                NotifyPropertyChanged("HexaDecimal");
+            }
+        }
+
+        public byte Green
+        {
+            get
+            {
+                return _green;
+            }
+            set
+            {
+                _green = value;
+                Colour = new SolidColorBrush(Color.FromArgb(255, Red, Green, Blue));
+                NotifyPropertyChanged();
+                NotifyPropertyChanged("MergedDecimal");
+                NotifyPropertyChanged("HexaDecimal");
+            }
+        }
+
+        public byte Blue
+        {
+            get
+            {
+                return _blue;
+            }
+            set
+            {
+                _blue = value;
+                Colour = new SolidColorBrush(Color.FromArgb(255, Red, Green, Blue));
+                NotifyPropertyChanged();
+                NotifyPropertyChanged("MergedDecimal");
+                NotifyPropertyChanged("HexaDecimal");
+            }
+        }
+
+        public SolidColorBrush Colour
+        {
+            get
+            {
+                return _color;
+            }
+            set
+            {
+                _color = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public string MergedDecimal
+        {
+            get
+            {
+                return String.Format("({0},{1},{2})",Red,Green,Blue);
+            }
+        }
+
+        public string HexaDecimal
+        {
+            get
+            {
+                string r = Red.ToString("X");
+                string g = Green.ToString("X");
+                string b = Blue.ToString("X");
+                return "#" + r.PadLeft(2, '0') + g.PadLeft(2, '0') + b.PadLeft(2, '0');
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
